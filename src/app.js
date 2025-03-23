9c53377d901e5a1c764bd66997ae596df0d2dbab
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2025-03-23 22:11:18 +0100

    feat: add env configuration

--- src/app.js
+++ src/app.js
@@ -2,11 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Subscription Tracking API' });
+  res.status(200).send('Welcome to the Subscription Tracking API');
 });
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000 at http://localhost:3000');
-})
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import app from './app.js';
 import 'dotenv/config';
 
-const PORT = process.env.PORT || 3000;
+import { PORT } from '../config/.env.js';
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
