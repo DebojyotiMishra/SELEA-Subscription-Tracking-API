5f00d8fa0775502b17ea0c8a18f1399f83d9c098
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2025-03-24 14:31:21 +0100

    feat: add user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,11 @@
 import { Router } from 'express';
+import { getUsers, getUserById } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
-  res.send({ message: 'GET all users' });
-});
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) => {
-  res.send({ message: `GET user with id ${req.params.id}` });
-});
+userRouter.get('/:id', getUserById);
 
 userRouter.post('/', (req, res) => {
   res.send({ message: 'POST a new user' });
